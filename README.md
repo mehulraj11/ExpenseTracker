994f967a4238a1245aa3cd1e0151861a74eedf08
mehulraj11 <mailtomehulraj@gmail.com>
2024-06-20 23:45:47 +0530

    readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# Features
+### add money on the basis of income and expense
+# left out
+### render each transaction as component
+### edit and delete transaction option
+### monthly total expense
---
+++ src/App.jsx
@@ -0,0 +1,140 @@
+import { useState } from "react";
+import "./App.css";
+
+export default function App() {
+  const [add, setAdd] = useState(true);
+  const [details, setDetails] = useState({
+    amount: "",
+    description: "",
+    type: "", 
+  });
+  const [transactions, setTransactions] = useState([]);
+  const [balance, setBalance] = useState(0);
+  const [income, setIncome] = useState(0);
+  const [expense, setExpense] = useState(0);
+
+  const handleAddClick = () => {
+    setAdd(!add);
+  };
+
+  const handleChangeInDetails = (e) => {
+    const { name, value } = e.target;
+    setDetails((prevDetails) => ({
+      ...prevDetails,
+      [name]: value,
+    }));
+  };
+
+  const handleTransactionTypeChange = (e) => {
+    setDetails((prevDetails) => ({
+      ...prevDetails,
+      type: e.target.value,
+    }));
+  };
+
+  const addValue = () => {
+    const amount = parseFloat(details.amount);
+    if (details.type === "expense") {
+      setBalance((prevBalance) => prevBalance - amount);
+      setExpense((prevExpense) => prevExpense + amount);
+    } else if (details.type === "income") {
+      setBalance((prevBalance) => prevBalance + amount);
+      setIncome((prevIncome) => prevIncome + amount);
+    }
+    setTransactions((prevTransactions) => [...prevTransactions, details]);
+    setDetails({
+      amount: "",
+      description: "",
+      type: "",
+    });
+  };
+
+  return (
+    <div className="app">
+      <div className="heading">
+        <h4>EXPENSE TRACKER</h4>
+      </div>
+      <div className="balance">
+        <div>
+          <p>
+            Balance Rs.<span style={{ color: "green" }}>{balance}</span>
+          </p>
+        </div>
+        <div>
+          <button className="add" onClick={handleAddClick}>
+            {add ? "Cancel" : "Add"}
+          </button>
+        </div>
+      </div>
+      {add && (
+        <div className="addTransaction">
+          <div className="formElement">
+            <input
+              type="number"
+              placeholder="Amount"
+              value={details.amount}
+              onChange={handleChangeInDetails}
+              name="amount"
+            />
+          </div>
+          <div className="formElement">
+            <input
+              type="text"
+              placeholder="Description"
+              value={details.description}
+              onChange={handleChangeInDetails}
+              name="description"
+            />
+          </div>
+          <div className="">
+            <label>
+              <input
+                type="radio"
+                value="expense"
+                checked={details.type === "expense"}
+                onChange={handleTransactionTypeChange}
+              />
+              Expense
+            </label>
+            <label>
+              <input
+                type="radio"
+                value="income"
+                checked={details.type === "income"}
+                onChange={handleTransactionTypeChange}
+              />
+              Income
+            </label>
+          </div>
+          <button onClick={addValue}>Add Transaction</button>
+        </div>
+      )}
+
+      <div className="display">
+        <div className="transactions">
+          <h4>Transactions</h4>
+          <ul>
+            {transactions.map((transaction, index) => (
+              <li key={index}>
+                <span>{transaction.description}</span> -
+                <span>Rs. {transaction.amount}</span>({transaction.type})
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="expense">
+          <div className="displayExpense">Expense</div>
+          <div className="displayMoney">
+            Rs.<span style={{ color: "red" }}>{-expense}</span>
+          </div>
+        </div>
+        <div className="income">
+          <div className="displayExpense">Income</div>
+          <div className="displayMoney">
+            Rs.<span style={{ color: "green" }}>{income}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
